e39261cca563bed7ae5f107140387e563aa7cd6f
DEVELOP\rahme <mohrahmatullah17051992@gmail.com>
2022-10-12 19:46:50 +0700

    commit

--- resources/views/eksternal/includes/header.blade.php
+++ resources/views/eksternal/includes/header.blade.php
@@ -1,7 +1,7 @@
 
    
   <!-- Page Header-->
-  <header class="masthead" style="background-image: url('eksternal/assets/img/home-bg.jpg')">
+  <header class="masthead" @if(!Request::is('detail/*')) style="background-image: url('eksternal/assets/img/home-bg.jpg')" @else style="background-image: url('../eksternal/assets/img/home-bg.jpg')" @endif>
       <div class="container position-relative px-4 px-lg-5">
           <div class="row gx-4 gx-lg-5 justify-content-center">
               <div class="col-md-10 col-lg-8 col-xl-7">
@@ -10,7 +10,7 @@
                       <h1>Lorem Ipsum</h1>
                       <span class="subheading">Lorem Ipsum is simply dummy text of the printing and typesetting industry</span>
                   </div>
-                  @endif
+                @endif
               </div>
           </div>
       </div>
