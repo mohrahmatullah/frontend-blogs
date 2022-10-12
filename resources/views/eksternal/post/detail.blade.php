4bc682fd2fb6ec847a8405f6e324f7a88436d99f
DEVELOP\rahme <mohrahmatullah17051992@gmail.com>
2022-10-12 14:18:45 +0700

    update

--- resources/views/eksternal/post/detail.blade.php
+++ resources/views/eksternal/post/detail.blade.php
@@ -2,18 +2,18 @@
 @section('title', 'Blog')
 @section('content')
 <!-- Post Content-->
-<article class="mb-4">
+<article class="mb-5">
     <div class="container px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <h2 class="post-title">{{ $table['title'] }}</h2>
                 <p>{{$table['body']}}</p>
-                <h5 class="post-title">Category</h5>
-                <a type="button" href="{{route('post-category', $table['category']['id'])}}" class="btn btn-outline-info btn-sm">{{ $table['category']['title'] }}</a>
-                <h5 class="post-title">Tag</h5>
+                <h5 class="post-title mt-5">Category</h5>
+                <a type="button" href="{{route('post-category', $table['category']['id'])}}" class="btn btn-outline-dark btn-sm">{{ $table['category']['title'] }}</a>
+                <h5 class="post-title mt-3">Tag</h5>
                 @foreach($tag as $row)
                     @if(in_array($row['id'], json_decode($table['tag_id'])) )
-                    <button type="button" class="btn btn-outline-danger btn-sm">{{$row['title']}}</button>
+                    <button type="button" class="btn btn-outline-dark btn-sm">{{$row['title']}}</button>
                     @endif
                 @endforeach
             </div>
